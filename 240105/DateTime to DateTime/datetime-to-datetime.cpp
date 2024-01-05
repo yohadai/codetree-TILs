39c6b4e03ace5fbc0220719251ef617bc51b9770
Yangmal <135188756+yohadai@users.noreply.github.com>
2024-01-05 17:38:26 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240105/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int d, h, m;
+	short lastDays[13]{ 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	cin >> d >> h >> m;
+	if (d < 11 || h < 11 || m < 11) {
+		cout << "-1";
+		return 0;
+	}
+
+	cout << (d - 11) * 60 * 24 + (h - 11) * 60 + (m - 11);
+
+	return 0;
+}

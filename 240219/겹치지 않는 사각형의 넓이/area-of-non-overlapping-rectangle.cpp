369d954e1465a4962d33223fef5fb2a2ce7e3903
Yangmal <135188756+yohadai@users.noreply.github.com>
2024-02-19 17:43:36 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240219/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -0,0 +1,39 @@
+#include <iostream>	
+
+int main() {
+	bool coordinate[201][201]{};
+
+	/*int n{};
+	std::cin >> n;*/
+	int x1[3]{}, y1[3]{}, x2[3]{}, y2[3]{};
+
+
+
+	for (int i{}; i < 2; ++i) {
+		std::cin >> x1[i] >> y1[i] >> x2[i] >> y2[i];
+		for (int x{ x1[i]}; x < x2[i]; ++x) {
+			for (int y{ y1[i]}; y < y2[i]; ++y) {
+				coordinate[x + 100][y + 100] = true;
+			}
+		}
+	}
+
+	std::cin >> x1[2] >> y1[2] >> x2[2] >> y2[2];
+	for (int x{ x1[2] }; x < x2[2]; ++x) {
+		for (int y{ y1[2] }; y < y2[2]; ++y) {
+			coordinate[x + 100][y + 100] = false;
+		}
+	}
+
+	int counter{};
+	for (int x{ 0 }; x < 201; ++x) {
+		for (int y{ 0 }; y < 201; ++y) {
+			if (coordinate[x][y] == true) {
+				counter++;
+			}
+		}
+	}
+
+	std::cout << counter;
+	return 0;
+}

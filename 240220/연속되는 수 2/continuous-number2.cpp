de3ca93fea944047f5a8d1dbc8b7ad40fca1b506
Yangmal <135188756+yohadai@users.noreply.github.com>
2024-02-20 23:00:19 +0900

    Title: 연속되는 수 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240220/연속되는 수 2/continuous-number2.cpp
+++ 240220/연속되는 수 2/continuous-number2.cpp
@@ -2,21 +2,21 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int counter{}; int maxCount{};
+    int counter{1}; int maxCount{1};
     int n;
     std::cin >> n;
     int numbers[1'000]{};
     for (int i{}; i < n; ++i){
         std::cin >> numbers[i];
     }
-    for (int i{}; i < n; ++i){
-        if (i == 0 || numbers[i] == numbers[i-1])
+    for (int i{1}; i < n; ++i){
+        if (numbers[i-1] == numbers[i]){
             ++counter;
-            if (maxCount < counter)
+            if (maxCount < counter){
                 maxCount = counter;
+            }
+        }
         else {
-            if (maxCount < counter)
-                maxCount = counter;
             counter = 1;
         }
     }

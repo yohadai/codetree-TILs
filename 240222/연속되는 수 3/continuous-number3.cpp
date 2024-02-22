9997a380e3736e2d18765ff05390cf7b72419667
Yangmal <135188756+yohadai@users.noreply.github.com>
2024-02-22 19:51:04 +0900

    Title: 연속되는 수 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240222/연속되는 수 3/continuous-number3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#define SIZE 1'000
+
+bool isSameSign(int a, int b){
+    if ( a * b >= 1){
+        return true;
+    }
+    else
+        return false;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int numbers[SIZE]{};
+    int n{};
+    std::cin >> n;
+    for (int i{}; i < n; ++i){
+        std::cin >> numbers[i];
+    }
+
+    int maxLength{1};
+    int length{};
+    for (int i{1}; i < n; ++i){
+        if (isSameSign(numbers[i - 1], numbers[i])){
+            length++;
+            if (length > maxLength){
+                maxLength = length;
+            }
+        }
+        else {
+            length = 1;
+        }
+    }
+
+    std::cout << maxLength << std::endl;
+    return 0;
+}

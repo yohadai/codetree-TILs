289a687fd2b89cfc5fbe80b246865d78397f7f3f
Yangmal <135188756+yohadai@users.noreply.github.com>
2026-02-25 00:51:32 +0900

    Title: 데이터센터의 온도 조정 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260225/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,14 @@
+N, C, G, H = map(int, input().split())
+ranges = [tuple(map(int, input().split())) for _ in range(N)]
+
+# Please write your code here.
+ans = 0
+for T in range(25):
+    work = 0
+    for machine in ranges:
+        if T < machine[0]: work += C
+        elif T < machine[1]: work += G
+        else: work += H
+    ans = max(ans, work)
+
+print(ans)

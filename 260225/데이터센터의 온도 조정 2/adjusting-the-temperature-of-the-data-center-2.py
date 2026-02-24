3c283e59dd1bfd461fdc343fbcae601d181f09ed
Yangmal <135188756+yohadai@users.noreply.github.com>
2026-02-25 00:54:53 +0900

    Title: 데이터센터의 온도 조정 2, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 260225/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
+++ 260225/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -7,7 +7,7 @@ for T in range(1010):
     work = 0
     for machine in ranges:
         if T < machine[0]: work += C
-        elif T < machine[1]: work += G
+        elif T <= machine[1]: work += G
         else: work += H
     ans = max(ans, work)
 

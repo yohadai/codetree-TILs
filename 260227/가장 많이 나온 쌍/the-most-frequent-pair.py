79a575f12a625cf450ab1c55f0b0b5ea22370b2f
Yangmal <135188756+yohadai@users.noreply.github.com>
2026-02-27 18:07:18 +0900

    Title: 가장 많이 나온 쌍, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260227/가장 많이 나온 쌍/the-most-frequent-pair.py
+++ 260227/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -1,15 +1,13 @@
 n, m = map(int, input().split())
-pairs = [set(map(int, input().split())) for _ in range(m)]
+pairs = [tuple(map(int, input().split())) for _ in range(m)]
 
 # Please write your code here.
 ans = 0
-for a in range(n-1):
-    for b in range(a+1, n):
+for a in range(1, n):
+    for b in range(a+1, n+1):
         cnt = 0
         for pair in pairs:
-            if set((a, b)) == pair: 
+            if (a, b) == pair or (b, a) == pair:
                 cnt += 1
         ans = max(ans, cnt)
-
-
 print(ans)

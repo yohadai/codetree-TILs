1f321b21e8da9f08598aa3f95af8fa296662210a
Yangmal <135188756+yohadai@users.noreply.github.com>
2026-02-28 23:15:06 +0900

    Title: 훌륭한 점프, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260228/훌륭한 점프/great-jump.py
+++ 260228/훌륭한 점프/great-jump.py
@@ -1,9 +1,10 @@
+
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 def is_possible(max_value):
     for i in range(n-k):
-        if arr[0] > max_value: return False
+        if arr[0] > max_value or arr[-1] > max_value: return False
         possible = False
         for j in range(i, i+k):
             if arr[j] <= max_value: 

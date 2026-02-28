323c195f10842029e1c1ebb15d3a9409885bf885
Yangmal <135188756+yohadai@users.noreply.github.com>
2026-03-01 00:34:22 +0900

    Title: 최대 최소간의 차, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260301/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -0,0 +1,15 @@
+import sys
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+# if not num in range(n, n+k): total += min(abs(num-n), abs(num-n-k))
+ans = sys.maxsize
+for num in range(1, 101):
+    cost = 0
+    for i in range(n):
+        if not num <= arr[i] <= num+k:
+            if num > arr[i]: cost += num - arr[i]
+            elif arr[i] > num+k: cost += arr[i] - num-k
+    ans = min(cost, ans)
+print(ans)

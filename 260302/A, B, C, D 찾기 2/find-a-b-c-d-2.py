0fb4435ad70c92c7307a2eeedf0b5bf078c07bd9
Yangmal <135188756+yohadai@users.noreply.github.com>
2026-03-02 14:04:03 +0900

    Title: A, B, C, D 찾기 2, Time: 111ms, Memory: 19MB, Status: Passed - Codetree

--- 260302/A, B, C, D 찾기 2/find-a-b-c-d-2.py
+++ 260302/A, B, C, D 찾기 2/find-a-b-c-d-2.py
@@ -8,13 +8,15 @@ def is_same_list(arr):
         if x != y: return False
     return True
 
-for A in range(len(nums)-3):
-    for B in range(A, len(nums)-2):
-        for C in range(B, len(nums)-1):
-            for D in range(C, len(nums)):
+for A in range(40-3):
+    for B in range(A, 40-2):
+        for C in range(B, 40-1):
+            for D in range(C, 40):
                 arr = [
                     A, B, C, D, A+B, B+C, C+D, 
                     D+A, A+C,B+D, A+B+C, A+B+D, A+C+D, 
                     B+C+D, A+B+C+D
                     ]
-                if is_same_list(arr): print(A, B,C, D)
+                if is_same_list(arr): 
+                    print(A, B,C, D)
+                    break

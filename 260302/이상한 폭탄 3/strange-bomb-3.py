a65d6f6f595393e2952bb5b8de8e9c98c449fab7
Yangmal <135188756+yohadai@users.noreply.github.com>
2026-03-02 22:07:19 +0900

    Title: 이상한 폭탄 3, Time: 59ms, Memory: 24MB, Status: Wrong Answer - Codetree

---
+++ 260302/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,20 @@
+N, K = map(int, input().split())
+num = [int(input()) for _ in range(N)]
+
+# 길이 k의 구간으로 완전탐색 진행, 새 배열에 터지는 폭탄의 번호 저장 후 제일 많은거 고르기
+
+explose = [False] * N
+for i in range(N-K):
+    for j in range(i+1, i+K+1):
+        if num[j] == num[i]: explose[i], explose[j] = True, True
+
+bomb = [0] * 1000000
+for i in range(N):
+    if explose[i] == True:
+        bomb[num[i]] += 1
+num = list(set(num))
+ans = num[0]
+for i in range(1, len(num)):
+    if bomb[num[i]] > bomb[ans]:
+        ans = num[i]
+print(ans)

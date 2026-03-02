3904759c9db8e29818bd2ee2ce03db12d4bfa712
Yangmal <135188756+yohadai@users.noreply.github.com>
2026-03-02 15:56:29 +0900

    Title: 독서실의 거리두기 4, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 260302/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
+++ 260302/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -1,25 +1,24 @@
 N = int(input())
-seat = list(map(int, list(input())))
+seat = list(map(int, input()))
 
-# Please write your code here.
-def dist(i, j):
-    str = seat[:]
-    str[i] == 1
-    str[j] == 1
-    min_dist = 1000000
-    dist = min_dist
-    for d in range(N):
-        if str[d] == 1:
-            min_dist = min(min_dist, dist)
-            dist = 0
-        else: dist += 1
+def min_dist(seat):
+    min_dist = 100000
+    prev = None
+    for i in range(N):
+        if seat[i] == 0: continue
+        else:
+            if prev == None: 
+                prev = i
+                continue
+            min_dist = min(min_dist, i - prev)
+            prev = i
     return min_dist
 
 ans = 0
 for i in range(N-1):
-    for j in range(N):
-        if i == j: continue
-        if seat[i] == 1 or seat[j] == 1: continue
-        ans = max(ans, dist(i, j))
-
+    for j in range(i+1, N):
+        if seat[i] == 1 or seat[j] == 1: continue 
+        tmp_seat = seat[:]
+        tmp_seat[i], tmp_seat[j] = 1, 1
+        ans = max(ans, min_dist(tmp_seat))
 print(ans)

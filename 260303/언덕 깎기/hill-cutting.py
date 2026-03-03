a4cd3ce8cd14db7ed34269ad97d2d4ab2dc7ff7c
Yangmal <135188756+yohadai@users.noreply.github.com>
2026-03-03 13:47:57 +0900

    Title: 언덕 깎기, Time: 86ms, Memory: 17MB, Status: Passed - Codetree

--- 260303/언덕 깎기/hill-cutting.py
+++ 260303/언덕 깎기/hill-cutting.py
@@ -8,5 +8,11 @@ heights = [int(input()) for _ in range(N)]
 ans = MAX_INT
 for min_h in range(101 - 17):
     cost = 0
-    for hight in heights:
-        
+    for height in heights:
+        if height not in range(min_h, min_h + 18):
+            carved = min(abs(min_h - height), abs(min_h + 17 - height))
+            cost += carved ** 2
+    ans = min(ans, cost)
+
+print(ans)
+

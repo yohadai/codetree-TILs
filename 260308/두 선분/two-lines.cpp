1ffb1ca949ee79e2549261249507707d2151febe
Yangmal <135188756+yohadai@users.noreply.github.com>
2026-03-08 22:46:13 +0900

    Title: 두 선분, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260308/두 선분/two-lines.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[4];
+
+int main() {
+    for (int i{}; i < 4; ++i) {
+        cin >> arr[i];
+    }
+
+    for (int i{}; i < 4; ++i) {
+        if (i == 0 || i == 1) {
+            if (arr[i] >= arr[2] || arr[i] <= arr[3]) {
+                cout << "intersecting";
+                return 0;
+            }
+        }
+        else {
+            if (arr[0] <= arr[i] || arr[i] <= arr[1]) {
+                cout << "intersecting";
+                return 0;
+            }
+        }
+    }
+    cout << "nonintersecting";
+
+    return 0;
+}

c9e992564af16e198d81ec3576e155f2ea090e25
Yangmal <135188756+yohadai@users.noreply.github.com>
2026-03-08 22:47:34 +0900

    Title: 두 선분, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260308/두 선분/two-lines.cpp
+++ 260308/두 선분/two-lines.cpp
@@ -11,13 +11,13 @@ int main() {
 
     for (int i{}; i < 4; ++i) {
         if (i == 0 || i == 1) {
-            if (arr[i] >= arr[2] || arr[i] <= arr[3]) {
+            if (arr[i] >= arr[2] && arr[i] <= arr[3]) {
                 cout << "intersecting";
                 return 0;
             }
         }
         else {
-            if (arr[0] <= arr[i] || arr[i] <= arr[1]) {
+            if (arr[0] <= arr[i] && arr[i] <= arr[1]) {
                 cout << "intersecting";
                 return 0;
             }

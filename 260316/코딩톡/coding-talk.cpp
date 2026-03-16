ff7583c71ecf378a035a7cd0fd67cabdeb0724df
Yangmal <135188756+yohadai@users.noreply.github.com>
2026-03-16 15:10:05 +0900

    Title: 코딩톡, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260316/코딩톡/coding-talk.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int n, m, p;
+char c[100];
+int u[100];
+
+int main() {
+    cin >> n >> m >> p;
+
+    for (int i = 0; i < m; i++) {
+        cin >> c[i] >> u[i];
+    }
+    set<char> s{};
+    for (int i = p-1; i < m; ++i) { // p는 1부터 시작.
+        s.insert(c[i]);
+    }
+    for (int i = 'A'; i < 65 + n; ++i) {
+        if (!s.contains(i)) {
+            cout << char(i) << " ";
+        }
+    }
+    // 그 사람 이하의 사람들을 제외한 사람들은 전부 다 잖아?
+    // Please write your code here.
+
+    return 0;
+}

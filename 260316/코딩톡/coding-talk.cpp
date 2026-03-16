62c97d04a04395f1a02d12c9477c803ae8aba5b8
Yangmal <135188756+yohadai@users.noreply.github.com>
2026-03-16 15:21:33 +0900

    Title: 코딩톡, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 260316/코딩톡/coding-talk.cpp
+++ 260316/코딩톡/coding-talk.cpp
@@ -8,16 +8,18 @@ int u[100];
 
 int main() {
     cin >> n >> m >> p;
-
     for (int i = 0; i < m; i++) {
         cin >> c[i] >> u[i];
     }
-    if (u[p-1] == 0) {
+
+    const int msg_num = u[p-1];
+    if (msg_num == 0) { // 예외 1.
         return 0;
     }
     set<char> s{};
-    for (int i = p-1; i < m; ++i) { // p는 1부터 시작.
-        s.insert(c[i]);
+    for (int i = 0; i < m; ++i) { 
+        if (p-1 < i || u[i] == msg_num)
+            s.insert(c[i]);
     }
     for (int i = 'A'; i < 'A' + n; ++i) {
         if (!s.contains(i)) {
@@ -25,6 +27,8 @@ int main() {
         }
     }
     // 그 사람 이하의 사람들을 제외한 사람들은 전부 다 잖아?
+    // 예외 1. p번째 메시지를 모두 읽은 경우
+    // 예외 2. p번째 메시지를 읽은 사람의 수가 그 전 메시지를 읽은 사람의 수와 같은 경우
     // Please write your code here.
 
     return 0;
